a6e5c1d5050b42eae213b933b731f1bff30217b4
Paola.Quintanilla <74014.linares@gmail.com>
2021-03-26 10:32:45 -0300

    first commit

---
+++ Servicios.TramitesUmss/Models/Tramiterequisito.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Servicios.TramitesUmss.Models
+{
+    [Table("tramiterequisito")]
+    public partial class Tramiterequisito
+    {
+        [Key]
+        [Column("Requisito_idPasos", TypeName = "int(11)")]
+        public int RequisitoIdPasos { get; set; }
+        [Key]
+        [Column("Requisito_Ubicacion_idUbicacion", TypeName = "int(11)")]
+        public int RequisitoUbicacionIdUbicacion { get; set; }
+        [Key]
+        [Column("Tramite_idTramites", TypeName = "int(11)")]
+        public int TramiteIdTramites { get; set; }
+
+        [ForeignKey("RequisitoIdPasos,RequisitoUbicacionIdUbicacion")]
+        [InverseProperty("Tramiterequisito")]
+        public virtual Requisito Requisito { get; set; }
+        [ForeignKey(nameof(TramiteIdTramites))]
+        [InverseProperty(nameof(Tramite.Tramiterequisito))]
+        public virtual Tramite TramiteIdTramitesNavigation { get; set; }
+    }
+}
---
+++ Servicios.TramitesUmss/Models/Ventanilla.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Servicios.TramitesUmss.Models
+{
+    [Table("ventanilla")]
+    public partial class Ventanilla
+    {
+        public Ventanilla()
+        {
+            Requisito = new HashSet<Requisito>();
+        }
+
+        [Key]
+        [Column("id", TypeName = "int(11)")]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(45)]
+        public string Nombre { get; set; }
+
+        [InverseProperty("UbicacionIdUbicacionNavigation")]
+        public virtual ICollection<Requisito> Requisito { get; set; }
+    }
+}
