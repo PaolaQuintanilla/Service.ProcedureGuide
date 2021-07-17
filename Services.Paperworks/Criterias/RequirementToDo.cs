4aba7444215c59c491f8f0277e2f65431df22968
Paola.Quintanilla <74014.linares@gmail.com>
2021-07-17 09:31:55 -0400

    add requirement todo and order list

---
+++ Services.Paperworks/Criterias/RequirementToDo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Paperworks.Criterias
+{
+    public class RequirementToDo
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}
